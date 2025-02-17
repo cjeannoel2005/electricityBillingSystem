ce7a14e881938be7d2bf72956e9f381ca56849b2
cedric <149272209+cjeannoel2005@users.noreply.github.com>
2025-02-16 23:34:05 -0500

    i have add the customerRecords and have to fix the exception in the main thread relating to memory (could be caused by an infinite loop)

--- src/BillingAccount.java
+++ src/BillingAccount.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+
 public class BillingAccount {
 
-int [] customerRecords = new int
+    // you must create a method in order for the for loop to work
+// use the arraylist here for the customerRecords ;
+    // the storage for the customerRecords
+    // the print method for the customerRecords
+    public static void printCustomerRecords(){
+        ArrayList<Integer> customerRecords = new ArrayList<>(1000000000);
+        for (int i = 0 ; i < customerRecords.size(); i++ )
+        {
+            System.out.println(customerRecords);
+            if (customerRecords.size() == 111110) {
+                System.out.println("this is the end of the billing records");
+                break;
+
+            }
+
+        }
+    }
+
+
+  // the main method in the billing account
+    public void main(String[] args) {
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+    Scanner scan = new Scanner(System.in);
 
+    // try to print the customerRecords to see if they work
+        BillingAccount.printCustomerRecords();
+       // error when code run as the exception in thread main java.lang
 
     }
 }
