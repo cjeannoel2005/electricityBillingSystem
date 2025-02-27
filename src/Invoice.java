9be63e2e99bce1222ea5eec7b090689ae6b54a85
cedric <149272209+cjeannoel2005@users.noreply.github.com>
2025-02-26 23:57:28 -0500

    fiexd the retrieve and search method for the invoice but failed ( need to fix the errors )

--- src/Invoice.java
+++ src/Invoice.java
@@ -5,31 +5,33 @@ public class Invoice {
 
 
     private String customerDetails;
-    private double unitsConsumed ;
-    private  float rate;
+    private Double unitsConsumed;
+    private float rate;
     private int totalAmount;
 
 
     // the arraylist for the search and retrieval of the customer details
 
-     String <ArrayList> InvoiceRetrieval = new <ArrayList>(customerDetails);
+    ArrayList<Invoice> invoiceRetrieval = new ArrayList<>(10000);
+
     // the customerDetails getters and setters
     // getters for customerDetails.
-    public String getCustomerDetails(){
+    public String getCustomerDetails() {
         return customerDetails;
     }
+
     // the setters for customerDetails
-    public void setCustomerDetails(String customerDetails){
+    public void setCustomerDetails(String customerDetails) {
         this.customerDetails = customerDetails;
     }
 
     // the getter for unitsConsumed
-    public double getUnitsConsumed(){
+    public double getUnitsConsumed() {
         return unitsConsumed;
     }
 
     // the setters for unitsConsumed
-    public void setUnitsConsumed(double unitsConsumed){
+    public void setUnitsConsumed(double unitsConsumed) {
         this.unitsConsumed = unitsConsumed;
 
     }
@@ -39,29 +41,41 @@ public class Invoice {
         return rate;
     }
 
-    public int getTotalAmount(){
+    public int getTotalAmount() {
         return totalAmount;
     }
 
     // the setters for the rate and totalAmount
 
-    public void setTotalAmount(int totalAmount){
+    public void setTotalAmount(int totalAmount) {
         this.totalAmount = totalAmount;
     }
 
-    public void setRate(float rate){
+    public void setRate(float rate) {
         this.rate = rate;
     }
 
 
+
+// create a new void method to sent error message
+
+public void displayErrorMessage(){
+    System.out.println("this is an error");
+}
 }
 
 // create methods to search and retrieve customer invoices by the id and names
- public static Invoice searchProduct(String customerDetails ){
-    for( Invoice Invoice : InvoiceRetrieval  ){
-        if( Invoice.getCustomerDetails().equals(customerDetails) ){
-            return Invoice ;
-            // fix this part because the invoice should not be the return ;
+    public Double searchProduct(String customerDetails) {
+        for (Invoice Invoice : InvoiceRetrieval) {
+            if (Invoice.getCustomerDetails().equals(customerDetails)) {
+                return unitsConsumed;
+                // fix this part because the invoice should not be the return ;
+            }
+            else{
+                displayErrorMessage();
+                return null;
+            }
+
         }
     }
- }
+}
